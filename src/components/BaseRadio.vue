31e90bc0a53f148a72e4934cce6617b75f54c668
koregeo <koregm@gmail.com>
2021-02-13 01:38:28 +0400

    add label prop

---
+++ src/components/BaseRadio.vue
@@ -0,0 +1,20 @@
+<template>
+  <input
+      type="radio"
+      v-model="event.pets"
+      :value="1"
+      name="pets"
+    />
+  <label v-if="label">{{ label }}</label>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
